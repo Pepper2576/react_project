5a2c2db21572ca4cece33b884c0b64c8d6c138c1
Chris Pepper <pepper2576@gmail.com>
2024-12-02 10:21:21 +0000

    url update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />}></Route>
+          <Route path='/react_project' element={<Home />}></Route>
           <Route path='/portfolio' element={<Products />}></Route>
         </Routes>
         <Footer />
